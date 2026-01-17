7ef2092aac5760dd50cbeb4dca6d5d39064d6647
KeiroEmio <2077405059@qq.com>
2026-01-17 23:04:52 +0800

    fix sever

--- front1/server/config/config.js
+++ front1/server/config/config.js
@@ -9,8 +9,8 @@ module.exports = {
   mysql: {
     host: process.env.MYSQL_HOST || "localhost",
     port: process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : 3306,
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "",
+    user: process.env.MYSQL_USER || "pay0db",
+    password: process.env.MYSQL_PASSWORD || "12345678",
     database: process.env.MYSQL_DATABASE || "pay0db"
   },
   server: {
